c8b6343dd93a650233a956efcb8791478b296889
Nursoltan <Nursoltan_h@yahoo.com>
2012-01-19 02:07:55 +0200

    CF #103(Div2)

--- Codeforces/Anagram Search.cpp
+++ Codeforces/Anagram Search.cpp
@@ -13,30 +13,3 @@
 
 using namespace std;
 
-int main(){
-	
-	string s,p;
-	int n,m,i,j;
-	int x[30]={0};
-	
-	cin>>s>>p;
-	
-	n=s.size();
-	m=p.size();
-	
-	for(i=0; i<m; i++) x[p[i]-'a']++;
-	
-	int ret=0;
-	for(i=0; i+m-1<n; i++){
-	  bool ok=1;
-	  int a[30]={0};
-	  for(j=i; j<i+m; j++) if(s[j]!='?') a[s[j]-'a']++;
-	  
-	  for(j=0; j<26; j++) if(a[j]>x[j]) ok=0;
-	  if(ok) ret++;
-	}
-	
-	cout<<ret<<endl;
-	
-	return 0;
-}
